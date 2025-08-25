1028f3750faeff2534e7e718e4e6f5c46af41733
Samuel Legrain <samuel.legrain@bstorm.be>
2025-08-25 12:09:32 +0200

    Ajout Démo03

---
+++ Demo-03-NotionClasse/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Demo_03_NotionClasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Voiture laFerrari = new Voiture()
+            {
+                couleur = "Rouge",
+                immatriculation = "1-ABC-123",
+                nbRoues = 4
+            };
+
+            Console.WriteLine($"Voici ma petite twingo, elle est {laFerrari.couleur}, immatriculée {laFerrari.immatriculation}, avec ses {laFerrari.nbRoues} roues.");
+
+            Console.WriteLine("Je la présente à ma mère, qui l'appelle la fraise!");
+
+            Voiture laFraise = laFerrari;
+
+            Console.WriteLine("En colère, je pars avec et me crash...");
+            laFerrari.nbRoues--;
+
+            Console.WriteLine($"Après l'accident, elle est {laFerrari.couleur}, immatriculée {laFerrari.immatriculation}, avec ses {laFerrari.nbRoues} roues.");
+
+
+            Console.WriteLine($"Après l'accident, elle est {laFraise.couleur}, immatriculée {laFraise.immatriculation}, avec ses {laFraise.nbRoues} roues.");
+
+        }
+    }
+}
---
+++ Demo-03-NotionClasse/Voiture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_03_NotionClasse
+{
+    internal class Voiture
+    {
+        public string couleur;
+        public string immatriculation;
+        public int nbRoues;
+    }
+}
