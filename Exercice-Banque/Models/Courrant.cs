f87c43296586b1bd111361e146b6b999b651dda5
Samuel Legrain <samuel.legrain@bstorm.be>
2025-08-25 16:36:56 +0200

    Correction Exo p33 : Class + Propriétés

---
+++ Exercice-Banque/Models/Courrant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Banque.Models
+{
+    internal class Courrant
+    {
+        private double _ligneDeCredit;
+        public double LigneDeCredit
+        {
+            get { return _ligneDeCredit; }
+            set {
+                if (value < 0) return; //A remplacer par une exception
+                _ligneDeCredit = value;
+            }
+        }
+        public string Numero { get; set; }
+
+        public Personne Titulaire { get; set; }
+
+        public double Solde { get; private set; }
+
+        public void Depot(double montant) {
+            if (montant <= 0) return; //A remplacer par une exception
+            Solde += montant;
+        }
+        public void Retrait(double montant)
+        {
+            if (montant <= 0) return; //A remplacer par une exception
+            if (Solde - montant < -LigneDeCredit) return; //A remplacer par une exception
+            Solde -= montant;
+        }
+
+    }
+}
---
+++ Exercice-Banque/Program.cs
@@ -0,0 +1,34 @@
+﻿using Exercice_Banque.Models;
+using System.Text;
+
+namespace Exercice_Banque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Personne p = new()
+            {
+                Nom = "Legrain",
+                Prenom = "Samuel",
+                DateNaiss = new(1987, 9, 27)
+            };
+
+            Courrant c1 = new() { 
+                Numero = "BE01",
+                Titulaire = p,
+                LigneDeCredit = 50
+            };
+
+            c1.Depot(10_000);
+
+            Console.WriteLine($"Infos compte \"{c1.Numero}\":\nTitulaire : {c1.Titulaire.Nom} {c1.Titulaire.Prenom}\nSolde : {c1.Solde} €\nLigne de crédit : {c1.LigneDeCredit} €");
+
+            c1.Retrait(10_050);
+
+            Console.WriteLine($"Infos compte \"{c1.Numero}\":\nTitulaire : {c1.Titulaire.Nom} {c1.Titulaire.Prenom}\nSolde : {c1.Solde} €\nLigne de crédit : {c1.LigneDeCredit} €");
+        }
+    }
+}
