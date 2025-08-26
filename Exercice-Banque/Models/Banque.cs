f7717b53efed1037d3d067aca8ff39e35a86698e
Samuel Legrain <samuel.legrain@bstorm.be>
2025-08-26 12:00:50 +0200

    Correction Exercice P39 : Indexeur (Version List)

--- Exercice-Banque/Models/Banque.cs
+++ Exercice-Banque/Models/Banque.cs
@@ -8,18 +8,19 @@ namespace Exercice_Banque.Models
 {
     internal class Banque
     {
-        private Dictionary<string, Courrant> _comptes = new();
+        #region Version Dictionaire
+        /*private Dictionary<string, Courrant> _comptes = new();
         public string Nom { get; set; }
 
         public Courrant? this[string numeroCompte]
         {
             get
             {
-                /*foreach (KeyValuePair <string, Courrant> kvp in _comptes)
-                {
-                    if (kvp.Key == numeroCompte) return kvp.Value;
-                }
-                return null;*/
+                //foreach (KeyValuePair <string, Courrant> kvp in _comptes)
+                //{
+                //    if (kvp.Key == numeroCompte) return kvp.Value;
+                //}
+                //return null;
                 Courrant result;
                 _comptes.TryGetValue(numeroCompte, out result);
                 return result;
@@ -28,12 +29,43 @@ namespace Exercice_Banque.Models
 
         public void Ajouter(Courrant compte)
         {
-            _comptes.Add(compte.Numero,compte);
+            _comptes.Add(compte.Numero, compte);
         }
 
         public void Supprimer(string numero)
         {
-            if(!_comptes.Remove(numero)) return; //Gérer avec une exception au lieu du return
+            if (!_comptes.Remove(numero)) return; //Gérer avec une exception au lieu du return
+        } */
+        #endregion
+
+        #region Version List
+        private List<Courrant> _comptes = new();
+        public string Nom { get; set; }
+
+        public Courrant? this[string numeroCompte]
+        {
+            get
+            {
+                //foreach (Courrant compte in _comptes)
+                //{
+                //    if(compte.Numero == numeroCompte) return compte
+                //}
+                //return null;
+                return _comptes.FirstOrDefault(c => c.Numero == numeroCompte);
+            }
         }
+        public void Ajouter(Courrant compte)
+        {
+            if (_comptes.Contains(compte)) return; //Gérer avec une exception au lieu du return
+            _comptes.Add(compte);
+        }
+
+        public void Supprimer(string numero)
+        {
+            Courrant? compteASupprimer = this[numero];
+            if (compteASupprimer is null) return; //Gérer avec une exception au lieu du return
+            if (!_comptes.Remove(compteASupprimer)) return; //Gérer avec une exception au lieu du return
+        }
+        #endregion
     }
 }
