a2257d9fd9b11c10b6c23ef3bc2797cb5f042e80
Samuel Legrain <samuel.legrain@bstorm.be>
2025-08-26 11:49:52 +0200

    Correction Exercice P39 : Indexeur

---
+++ Exercice-Banque/Models/Banque.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Banque.Models
+{
+    internal class Banque
+    {
+        private Dictionary<string, Courrant> _comptes = new();
+        public string Nom { get; set; }
+
+        public Courrant? this[string numeroCompte]
+        {
+            get
+            {
+                /*foreach (KeyValuePair <string, Courrant> kvp in _comptes)
+                {
+                    if (kvp.Key == numeroCompte) return kvp.Value;
+                }
+                return null;*/
+                Courrant result;
+                _comptes.TryGetValue(numeroCompte, out result);
+                return result;
+            }
+        }
+
+        public void Ajouter(Courrant compte)
+        {
+            _comptes.Add(compte.Numero,compte);
+        }
+
+        public void Supprimer(string numero)
+        {
+            if(!_comptes.Remove(numero)) return; //Gérer avec une exception au lieu du return
+        }
+    }
+}
--- Exercice-Banque/Program.cs
+++ Exercice-Banque/Program.cs
@@ -7,6 +7,7 @@ namespace Exercice_Banque
     {
         static void Main(string[] args)
         {
+            #region Exercice 01
             Console.OutputEncoding = Encoding.Unicode;
 
             Personne p = new()
@@ -16,7 +17,8 @@ namespace Exercice_Banque
                 DateNaiss = new(1987, 9, 27)
             };
 
-            Courrant c1 = new() { 
+            Courrant c1 = new()
+            {
                 Numero = "BE01",
                 Titulaire = p,
                 LigneDeCredit = 50
@@ -29,6 +31,46 @@ namespace Exercice_Banque
             c1.Retrait(10_050);
 
             Console.WriteLine($"Infos compte \"{c1.Numero}\":\nTitulaire : {c1.Titulaire.Nom} {c1.Titulaire.Prenom}\nSolde : {c1.Solde} €\nLigne de crédit : {c1.LigneDeCredit} €");
+            #endregion
+
+            #region Exercice 02
+
+            Banque b = new() { 
+                Nom = "TFTIC"
+            };
+
+            b.Ajouter(c1);
+            b.Ajouter(new()
+            {
+                Numero = "BE02",
+                Titulaire = p,
+                LigneDeCredit = 100
+            });
+
+            b.Ajouter(new()
+            {
+                Numero = "BE03",
+                Titulaire = new()
+                {
+                    Nom = "Claes",
+                    Prenom = "Alexandre",
+                    DateNaiss = new(1991, 1, 1)
+                },
+                LigneDeCredit = 50
+            });
+
+            Console.WriteLine("Utilisateur, veuillez choisir un compte");
+            string numeroChoisi = Console.ReadLine() ?? "";
+            Courrant? compteChoisi = b[numeroChoisi];
+            if (compteChoisi is null)
+            {
+                Console.WriteLine($"Aucun compte ne correspond au numéro \"{numeroChoisi}\"...");
+            }
+            else {
+                Console.WriteLine($"Infos compte \"{compteChoisi.Numero}\":\nTitulaire : {compteChoisi.Titulaire.Nom} {compteChoisi.Titulaire.Prenom}\nSolde : {compteChoisi.Solde} €\nLigne de crédit : {compteChoisi.LigneDeCredit} €");
+            }
+
+            #endregion
         }
     }
 }
