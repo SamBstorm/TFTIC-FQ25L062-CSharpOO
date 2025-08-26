ac8493272df8f9f69aa4800ed0bdc2303d099604
Samuel Legrain <samuel.legrain@bstorm.be>
2025-08-26 12:34:18 +0200

    Ajout Démo 07

---
+++ Demo-07-Operateurs/Panier.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_07_Operateurs
+{
+    internal class Panier
+    {
+        private List<Fruit> _fruits = new();
+
+        public int Count { 
+            get {
+                return _fruits.Count;
+            } 
+        }
+
+        public void Ajouter(Fruit newFruit)
+        {
+            _fruits.Add(newFruit);
+        }
+
+        public static Panier operator +(Panier left, Panier right)
+        {
+            Panier result = new();
+            foreach (Fruit f in left._fruits)
+            {
+                result.Ajouter(f);
+            }
+            foreach (Fruit f in right._fruits)
+            {
+                result.Ajouter(f);
+            }
+
+            return result;
+        }
+
+        public static Panier operator + (Panier left, Fruit right)
+        {
+            left.Ajouter(right);
+            return left;
+        }
+
+        public static bool operator ==(Panier left, Panier right)
+        {
+            return left.Count == right.Count;
+        }
+
+        public static bool operator !=(Panier left, Panier right)
+        {
+            return left.Count != right.Count;
+        }
+
+        public static bool operator >(Panier left, int right)
+        {
+            return left.Count > right;
+        }
+        public static bool operator <(Panier left, int right)
+        {
+            return left.Count < right;
+        }
+    }
+}
---
+++ Demo-07-Operateurs/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Demo_07_Operateurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Panier p1 = new();
+
+            p1.Ajouter(new());
+            p1.Ajouter(new());
+            p1.Ajouter(new());
+
+            Panier p2 = new();
+            p2.Ajouter(new());
+            p2.Ajouter(new());
+            p2.Ajouter(new());
+            p2.Ajouter(new());
+
+            Console.WriteLine($"Dans mon panier, j'ai actuellement {p1.Count} fruits.");
+
+            Console.WriteLine($"Dans le panier de mon collègue, il a actuellement {p2.Count} fruits.");
+
+            Panier recolteTotal = p1 + p2;
+
+            Console.WriteLine($"Au total, nous avons récolté : {recolteTotal.Count} fruits!");
+
+            Console.WriteLine("Voici un nouveau fruit!");
+
+            //p1 = p1 + new Fruit();
+            p1 += new Fruit(); 
+            Console.WriteLine($"Dans mon panier, j'ai actuellement {p1.Count} fruits.");
+
+            if (p1 == p2)
+            {
+                Console.WriteLine("Nous avons le même nombre de fruits!");
+            }
+            else {
+                Console.WriteLine("Nous n'avons pas la même récolte...");
+            }
+        }
+    }
+}
