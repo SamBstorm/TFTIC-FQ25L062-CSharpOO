156aebf1b9ca62e1430d1a8fd01f7641eb25269c
Samuel Legrain <samuel.legrain@bstorm.be>
2025-08-28 10:09:43 +0200

    Ajout Exercice Static

---
+++ Exercice-Static/Dice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Static
+{
+    internal static class Dice
+    {
+        private static Random RNG = new();
+        private static int _minimum = 1;
+        private static int _maximum = 6;
+
+        public static int Minimum
+        {
+            get { return _minimum; }
+            set {
+                _minimum = value;
+                if (_maximum <= _minimum) _maximum = _minimum + 1;
+            }
+        }
+
+        public static int Maximum
+        {
+            get { return _maximum; }
+            set
+            {
+                _maximum = value;
+                if (_minimum >= _maximum) _minimum = _maximum - 1;
+            }
+        }
+
+        public static int Roll()
+        {            
+            return RNG.Next(Minimum, Maximum+1);
+        }
+
+        public static int[] Roll(int nb)
+        {
+            int[] rolls = new int[nb];
+            for (int i = 0; i < nb; i++)
+            {
+                rolls[i] = Roll();
+            }
+            return rolls;
+        }
+    }
+}
---
+++ Exercice-Static/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercice_Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice.Minimum = 1;
+            Dice.Maximum = 20;
+            Console.WriteLine(Dice.Roll());
+
+            Console.WriteLine($"Je lance 3 dés de {Dice.Minimum} à {Dice.Maximum} :");
+
+            foreach (int result in Dice.Roll(3))
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
