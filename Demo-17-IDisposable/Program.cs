6b290bea3f7c744370285e7f3bf0a6b0d14e2c1e
Samuel Legrain <samuel.legrain@bstorm.be>
2025-08-29 09:41:54 +0200

    Ajout Demo 17

---
+++ Demo-17-IDisposable/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Demo_17_IDisposable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (TalkieWalkie tw2 = new("Gérard"))
+            {
+                using (TalkieWalkie tw1 = new("Sam"))
+                {
+                    tw1.Parler("Bonjour!");
+                    tw2.Parler("Salut! Comment vas-tu?");
+                    tw1.Parler("Je vais bien et toi?");
+                    tw2.Parler("Ca va! mais je crois que l'on nous écoute!");
+                    tw1.Parler("Ok! A plus tard!");
+                    tw2.Parler("À plus!");
+                }
+            }
+        }
+    }
+}
---
+++ Demo-17-IDisposable/TalkieWalkie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Demo_17_IDisposable
+{
+    internal class TalkieWalkie : IDisposable
+    {
+        public string Nom { get; private set; }
+        public TalkieWalkie(string nom)
+        {
+            Nom = nom;
+            Console.WriteLine($"{Nom} est connectée.");
+        }
+
+        public void Parler(string text) {
+            Console.WriteLine($"{Nom} {DateTime.Now.ToShortTimeString()} : {text}");
+            Console.WriteLine("À vous!");
+        }
+
+        public void Dispose() {
+            Console.WriteLine($"{Nom} {DateTime.Now.ToShortTimeString()} : Terminé!");
+            Console.WriteLine($"{Nom} n'est plus en ligne...");
+        }
+    }
+}
