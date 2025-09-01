c4468f0f25f119ccf6373b38852c3e190b8c3280
Samuel Legrain <samuel.legrain@bstorm.be>
2025-09-01 16:01:48 +0200

    Ajout Exercice P127 : Les délégués

---
+++ Exercice-CarWash/Models/CarWash.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_CarWash.Models
+{
+    delegate void CarWashDelegate(Voiture v);
+    internal class CarWash
+    {
+        private CarWashDelegate? _traitement;
+
+        public CarWash()
+        {
+            /* Exercice 2 - Suppression des méthodes
+            _traitement = Preparer;
+            _traitement += Laver;
+            _traitement += Secher;
+            _traitement += Finaliser;*/
+            _traitement = delegate (Voiture v)
+            {
+                Console.WriteLine($"Je prépare la voiture : {v.Plaque}.");
+            };
+            _traitement += delegate (Voiture v)
+            {
+                Console.WriteLine($"Je lave la voiture : {v.Plaque}.");
+            };
+            _traitement += delegate (Voiture v)
+            {
+                Console.WriteLine($"Je sèche la voiture : {v.Plaque}.");
+            };
+            _traitement += delegate (Voiture v)
+            {
+                Console.WriteLine($"Je finalise la voiture : {v.Plaque}.");
+            };
+        }
+        /* Exercice 2 - Suppression des méthodes
+        private void Preparer(Voiture v)
+        {
+            Console.WriteLine($"Je prépare la voiture : {v.Plaque}.");
+        }
+        private void Laver(Voiture v)
+        {
+            Console.WriteLine($"Je lave la voiture : {v.Plaque}.");
+        }
+        private void Secher(Voiture v)
+        {
+            Console.WriteLine($"Je sèche la voiture : {v.Plaque}.");
+        }
+        private void Finaliser(Voiture v)
+        {
+            Console.WriteLine($"Je finalise la voiture : {v.Plaque}.");
+        }
+        */
+        public void Traiter (Voiture v)
+        {
+            _traitement?.Invoke(v);
+        }
+    }
+}
---
+++ Exercice-CarWash/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exercice_CarWash.Models;
+
+namespace Exercice_CarWash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Voiture v1 = new("1-ABC-123");
+            Voiture v2 = new("2-DEF-456");
+            Voiture v3 = new("0-GHI-789");
+
+            CarWash cw = new();
+
+            cw.Traiter(v1);
+            cw.Traiter(v2);
+            cw.Traiter(v3);
+        }
+    }
+}
