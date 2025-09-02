83235194f363ba24257fdf3f83279faae90f6ed8
phil-bstorm <philippe.haerens@bstorm.be>
2025-09-02 09:58:12 +0200

    Demo25-Evenement-02 ⚡

---
+++ Demo-25-Evenement-02/Models/Tamagochi.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_25_Evenement_02.Models
+{
+    public delegate void TamagochiFaimEvent(Tamagochi t);
+
+    public class Tamagochi
+    {
+
+        #region Field
+        protected int _JaugeFaim = 100;
+        #endregion
+
+        #region Constants
+        private const int FAIM_MIN = 0;
+        private const int FAIM_WARNING = 20;
+        private const int FAIM_MAX = 100;
+        #endregion
+
+        #region Events
+        public event TamagochiFaimEvent? FaimEvent = null;
+        #endregion
+
+        #region props
+        public string Nom;
+        public bool estVivant = true;
+        #endregion
+
+        public Tamagochi(string nom)
+        {
+            Nom = nom;
+        }
+
+        #region Methods
+        public void PasserJournee()
+        {
+            _JaugeFaim -= Random.Shared.Next(10, 30);
+            if(_JaugeFaim < FAIM_WARNING)
+            {
+                // appeler l'event
+                FaimEvent?.Invoke(this);
+            }
+
+            if(_JaugeFaim <= FAIM_MIN)
+            {
+                Console.WriteLine($"{Nom} est mort...");
+                estVivant = false;
+            }
+        }
+
+        public void Nourir()
+        {
+            if(Random.Shared.Next(0, 2) == 1)
+            {
+                Console.WriteLine("Faim + 20");
+                _JaugeFaim += 20;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Demo-25-Evenement-02/Program.cs
@@ -0,0 +1,30 @@
+﻿using Demo_25_Evenement_02.Models;
+
+Tamagochi cookie = new("Cookie");
+Tamagochi pasteque = new("Pasteque");
+
+void Reagir(Tamagochi m) {
+    Console.WriteLine("REAGITTT: " + m.Nom);
+    m.Nourir();
+}
+
+// abonnement à Event +=
+cookie.FaimEvent += Reagir;
+pasteque.FaimEvent += Reagir;
+
+cookie.FaimEvent += (Tamagochi m) =>
+{ // lambda expression
+    Console.WriteLine("Lambda " + m.Nom);
+};
+
+// désabonner d'un Event -=
+//cookie.FaimEvent -= Reagir;
+
+int journee = 0;
+while (cookie.estVivant)
+{
+    journee++;
+    Console.WriteLine($"Jour {journee}");
+    cookie.PasserJournee();
+    pasteque.PasserJournee();
+}
